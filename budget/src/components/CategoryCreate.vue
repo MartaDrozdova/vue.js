96a8beb8636a05fd6753ce6d740d71328483f58c
Marta Drozdova <martysik0803@gmail.com>
2020-02-10 18:48:09 +0200

    components categorie

---
+++ budget/src/components/CategoryCreate.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col s12 m6">
+          <div>
+            <div class="page-subtitle">
+              <h4>Створити</h4>
+            </div>
+
+            <form>
+              <div class="input-field">
+                <input id="name" type="text">
+                <label for="name">Назва</label>
+                <span class="helper-text invalid">Ведіть назву</span>
+              </div>
+
+              <div class="input-field">
+                <input id="limit" type="number">
+                <label for="limit">Ліміт</label>
+                <span class="helper-text invalid">Мінімальна величина</span>
+              </div>
+
+              <button class="btn waves-effect waves-light" type="submit">
+                Создать
+                <i class="material-icons right">send</i>
+              </button>
+            </form>
+          </div>
+        </div>
+</template>
